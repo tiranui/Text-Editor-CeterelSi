ea442d8b3d7d79fa34e5104ec70b970fa10db9d2
veloraalteir <rafliarauf63@gmail.com>
2026-04-05 22:19:52 +0700

    update fitur save & save as, then adding fiture like open &close file also making library a separate file from the main source code

---
+++ Rafli.h
@@ -0,0 +1,9 @@
+/*Library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+#define MAX_LINES 1000
+#define MAX_LENGTH 256
+#define VIEW_HEIGHT 20
--- main.c
+++ main.c
@@ -1,12 +1,4 @@
-/*Library*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <conio.h>
-
-#define MAX_LINES 1000
-#define MAX_LENGTH 256
-#define VIEW_HEIGHT 20
+#include "Rafli.h"
 /*Program*/
 /* Data Teks*/
 char text[MAX_LINES][MAX_LENGTH];
@@ -105,6 +97,7 @@ void editorRefreshScreen() {
         printf("========================\n");
     }
 }
+/*Program edit dan move cursor*/
 // ================= CURSOR =================
 void moveCursor(int key) {
     int len = strlen(text[cy]);
@@ -189,4 +182,155 @@ void deleteChar() {
         }
         cx--;
     }
+}
+/*fitue file*/
+// ================= FILE =================
+/*open file*/
+void openFile() {
+    char filename[100];
+    FILE *fp;
+
+    clearScreen();
+    printf("Open file: ");
+
+    fgets(filename, sizeof(filename), stdin);
+    if (filename[0] == '\n') fgets(filename, sizeof(filename), stdin);
+
+    filename[strcspn(filename, "\n")] = 0;
+
+    fp = fopen(filename, "r");
+
+    if (!fp) {
+        perror("Error");
+        system("pause");
+        return;
+    }
+
+    line_count = 0;
+
+    while (fgets(text[line_count], MAX_LENGTH, fp)) {
+        line_count++;
+    }
+
+    fclose(fp);
+    strcpy(currentFile, filename);
+
+    cy = 0;
+    cx = 0;
+    row_offset = 0;
+}
+/*save & save as*/
+void saveFile() {
+    int i;
+    FILE *fp;
+
+    if (strlen(currentFile) == 0) {
+        printf("Gunakan Save As dulu!\n");
+        system("pause");
+        return;
+    }
+
+    fp = fopen(currentFile, "w");
+
+    for (i = 0; i < line_count; i++) {
+        fprintf(fp, "%s", text[i]);
+    }
+
+    fclose(fp);
+}
+
+void saveAs() {
+    int i;
+    char filename[100];
+    FILE *fp;
+
+    clearScreen();
+    printf("Save As: ");
+
+    fgets(filename, sizeof(filename), stdin);
+    if (filename[0] == '\n') fgets(filename, sizeof(filename), stdin);
+
+    filename[strcspn(filename, "\n")] = 0;
+
+    fp = fopen(filename, "w");
+
+    for (i = 0; i < line_count; i++) {
+        fprintf(fp, "%s", text[i]);
+    }
+
+    fclose(fp);
+
+    strcpy(currentFile, filename);
+}
+/*close file*/
+
+void closeFile() {
+    line_count = 1;
+    text[0][0] = '\0';
+    currentFile[0] = '\0';
+    cx = cy = 0;
+}
+/*main program*/
+/*================= MAIN =================*/
+int main() {
+    int key;
+
+    text[0][0] = '\0';
+
+    while (1) {
+        editorRefreshScreen();
+        key = readKey();
+
+        if (mode == 0) {
+            if (key == 27) {
+                mode = 1;
+            }
+            else if (key == 13) {
+                insertNewLine();
+            }
+            else if (key == 8) {
+                deleteChar();
+            }
+            else if (key >= 32 && key <= 126) {
+                insertChar(key);
+            }
+            else {
+                moveCursor(key);
+            }
+        } else {
+            switch (key) {
+                case 'i':
+                case 'I':
+                    mode = 0;
+                    break;
+
+                case 'o':
+                case 'O':
+                    openFile();
+                    mode = 0;
+                    break;
+
+                case 's':
+                case 'S':
+                    saveFile();
+                    break;
+
+                case 'a':
+                case 'A':
+                    saveAs();
+                    break;
+
+                case 'c':
+                case 'C':
+                    closeFile();
+                    break;
+
+                case 'q':
+                case 'Q':
+                    return 0;
+            }
+        }
+    }
+
+    return 0;
 }
