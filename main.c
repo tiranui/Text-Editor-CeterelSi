a928bf1ad94c6ae27dacb0bfd3d5eed62ee72d24
veloraalteir <rafliarauf63@gmail.com>
2026-04-05 19:26:55 +0700

    update data teks & page up/page down

--- main.c
+++ main.c
@@ -7,3 +7,24 @@
 #define MAX_LINES 1000
 #define MAX_LENGTH 256
 #define VIEW_HEIGHT 20
+/*Program*/
+/* Data Teks*/
+char text[MAX_LINES][MAX_LENGTH];
+int line_count = 1;
+
+char currentFile[100] = "";
+
+int cx = 0, cy = 0;
+int row_offset = 0;
+
+int mode = 0; // 0 = EDIT, 1 = COMMAND
+/*Page up & Page up*/
+// ================= KEY =================
+enum keys {
+    ARROW_UP = 1000,
+    ARROW_DOWN,
+    ARROW_LEFT,
+    ARROW_RIGHT,
+    PAGE_UP,
+    PAGE_DOWN
+};
