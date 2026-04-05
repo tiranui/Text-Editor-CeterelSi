b273a25b721cffc007232bda4f08225bd710d8f7
veloraalteir <rafliarauf63@gmail.com>
2026-04-05 18:47:53 +0700

    Library update "Rafli"

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+/*Library*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 
-int main() {
-    printf("Hello world");
-    return 0;
-}
+#define MAX_LINES 1000
+#define MAX_LENGTH 256
+#define VIEW_HEIGHT 20
